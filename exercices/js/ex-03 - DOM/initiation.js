61f36ba2eff1a143eb42200cef4ccefaae7197c2
Thomas088 <thomaskouadio92@gmail.com>
2020-03-30 14:25:54 +0200

    Form 3 - Size

--- exercices/js/ex-03 - DOM/initiation.js
+++ exercices/js/ex-03 - DOM/initiation.js
@@ -1,8 +1,19 @@
+// Champ qui se vide lorsque l'on entre dedans et qui remet la valeur initiale si le champ est vide lorsqu'on le quitte
 
 document.forms[0].ch1.addEventListener('focus', () => {        
     document.forms[0].ch1.value="";
 })
 
+// Champ dont la taille augmente lorsque l'on appuye sur une touche et qu'on est dedans
+
 document.forms[1].ch1.addEventListener('focus', () => {
-    document.forms[1].ch1.value="test";
+    document.forms[1].ch1.value="";
 })
+
+document.forms[1].ch1.addEventListener('blur', () => {
+    if(document.forms[1].ch1.value === "") {
+        document.forms[1].ch1.value = "Texte initial";
+    }
+})
+
+// Champ dont la taille augmente lorsque l'on appuye sur une touche et qu'on est dedans
