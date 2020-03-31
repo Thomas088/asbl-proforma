64a2ddc9b581b3bb3e7f13a8ab8d192341c96bb0
Thomas088 <thomaskouadio92@gmail.com>
2020-03-31 16:59:28 +0200

    Form 11

--- exercices/js/ex-03 - DOM/initiation.js
+++ exercices/js/ex-03 - DOM/initiation.js
@@ -14,16 +14,16 @@ document.forms[1].ch1.addEventListener('blur', () => {
     
     ((e) => {
         e.ch1.value === "" ? e.ch1.value = "Texte initial" : 0;
-    })(document.forms[1]);
+    })(document.forms[1])
 
-});
+})
 
 // La taille augmente lorsque l'on appuye sur une touche et qu'on est dedans
 
 document.forms[2].ch1.value = "";
 
 document.forms[2].ch1.addEventListener('keydown', () => {  
-    document.forms[2].ch1.size++;
+    document.forms[2].ch1.size++
 });
 
 // Affiche les caractères quand la case est cochée, les masque sinon
@@ -33,7 +33,7 @@ document.forms[3].cb1.addEventListener('click', () => {
     ((e) => {
         e.ch1.type === "password" ? e.ch1.type = "text" : e.ch1.type = "password";        
         }
-    )(document.forms[3]);
+    )(document.forms[3])
 
 });
 
@@ -83,18 +83,18 @@ document.forms[6].cb1.addEventListener('click', () => {
 // Un champ non modifiable qui s'incrémente ou se décrémente selon l'option sélectionnée
 
 let entryForm7;
-
 document.forms[7].ch1.setAttribute("readonly" , "");
-
 document.forms[7].ch1.value = 0;
-
 document.forms[7].b1.addEventListener('click', () => {
 
     entryForm7 = Number(document.forms[7].ch1.value);
 
     ((e) => {
-        document.querySelector("#rb1_1").checked ? e.ch1.value = entryForm7 + 1 : document.querySelector("#rb1_2").checked ? e.ch1.value = entryForm7 - 1 : 0;
-    })(document.forms[7])
+        document.querySelector("#rb1_1").checked ? 
+        e.ch1.value = entryForm7 + 1 : 
+        document.querySelector("#rb1_2").checked ? 
+        e.ch1.value = entryForm7 - 1 : 0;
+    })(document.forms[7]);
     
 })
 
@@ -102,7 +102,7 @@ document.forms[7].b1.addEventListener('click', () => {
 // Le bouton "reset" fait son boulot...
 
 
-let entryForm8
+let entryForm8;
 
 document.forms[8].b1.addEventListener('click', () => {
 
@@ -124,10 +124,18 @@ document.forms[8].b2.addEventListener('click', () => {
 document.forms[9].ch1.value = "";
 document.forms[9].ch2.value = "";
 
-  Array.from(document.forms[9].s1.selectedOptions).forEach((option) => {
+document.forms[9].addEventListener("change", () => {
+
+      ((e) => {    
+      Array.from(e.s1.options).forEach((option) => {
+        if(option.selected) {
+            e.ch1.value = option.value;
+            e.ch2.value = option.text;
+        }
+      }) 
+    } )(document.forms[9])
+})
+
+// Place les valeurs et les textes des options sélectionnées à l'intérieur des champs correspondants
 
 
-
-  })
-
-console.log(Array.from(document.forms[9].s1.selectedOptions))
