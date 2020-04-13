f7d3d8c1f2e6335d3151c4f87c39d691d04563b4
Thomas <thomaskouadio92@gmail.com>
2020-04-13 18:12:01 +0200

    [js] add unique-colors-array

--- exercices/js/ex-04 - Mastermind/mastermind.js
+++ exercices/js/ex-04 - Mastermind/mastermind.js
@@ -1,10 +1,31 @@
-window.addEventListener('DOMContentLoaded', game)
-
-
 function game() {
 
-    
+   const colors = ["green", "blue", "purple", "gold", "brown", "teal", "pink"];
 
+   function randomChoiceColor()  {
+      return colors[Math.round(Math.random() * Math.round(colors.length - 1))];
+   }
+   
+   function arrayOfColorsPC() {
 
+      let array = [];
+      let i = 0;
 
+      do {
+         let value = randomChoiceColor()  
+         // Verifie les doublons
+         if(array.indexOf(value) === -1) { 
+            array.push(value);
+         } else { // Si la valeur existe, on revient en arrière d'un index.
+            i-=1;
+         }
+        i++;   
+      } while(i < 4);
+      return array;
+   }
+
+  const choicePC = arrayOfColorsPC();
+  console.log(choicePC);
 }
+
+window.addEventListener('DOMContentLoaded', game)
